af5879a34b266f0068ef5baa808f07fd0871a8da
Gonzalo Uceda Castro <gonzaloucedacastro@Gonzalos-MacBook-Pro.local>
2020-09-10 10:25:46 +0200

    remove unnecesary files and rename

--- CREDENTIALS.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-        pusher: {
-                appId: '936500',
-                key: '7115a2da7069d51ec7ee',
-                secret: 'e3eec3af384d30294dae',
-                cluster: 'eu',
-                encrypted: true
-        }
-}
